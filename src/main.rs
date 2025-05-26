c0a4e5ecbda09d7b484ae7b18f8bd31f7f4dc2c2
Igor Martayan <igor@martayan.org>
2025-05-26 17:13:27 +0200

    Handle Ns in fasta files

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use clap::Parser;
 use crossbeam_channel::unbounded;
 use needletail::parse_fastx_file;
 use packed_seq::{PackedSeqVec, Seq, SeqVec};
+use regex::bytes::RegexBuilder;
 use rustc_hash::FxHashSet as HashSet;
 
 #[derive(Parser, Debug)]
@@ -112,17 +113,24 @@ fn index_reference_kmers<P: AsRef<Path>>(
 ) -> io::Result<HashSet<u64>> {
     let mut dict = HashSet::default();
     let mut reader = parse_fastx_file(ref_path).expect("Failed to parse reference file");
+    let match_n = RegexBuilder::new(r"[N]+")
+        .case_insensitive(true)
+        .unicode(false)
+        .build()
+        .unwrap();
 
     while let Some(result) = reader.next() {
         let record = result.map_err(io::Error::other)?;
         let seq = &record.seq();
-        // for hash in nthash::nthash(seq, kmer_size) {
-        //     dict.insert(hash);
-        // }
-        let hasher = ahash::RandomState::with_seed(42);
-        for kmer in seq.windows(kmer_size) {
-            dict.insert(hasher.hash_one(kmer));
-        }
+        match_n.split(seq).for_each(|seq| {
+            // for hash in nthash::nthash(seq, kmer_size) {
+            //     dict.insert(hash);
+            // }
+            let hasher = ahash::RandomState::with_seed(42);
+            for kmer in seq.windows(kmer_size) {
+                dict.insert(hasher.hash_one(kmer));
+            }
+        });
     }
     Ok(dict)
 }
@@ -135,27 +143,34 @@ fn index_reference_minimizers<P: AsRef<Path>>(
     let window_size: usize = kmer_size - minimizer_size + 1;
     let mut dict = HashSet::default();
     let mut reader = parse_fastx_file(ref_path).expect("Failed to parse reference file");
+    let match_n = RegexBuilder::new(r"[N]+")
+        .case_insensitive(true)
+        .unicode(false)
+        .build()
+        .unwrap();
 
     while let Some(result) = reader.next() {
         let record = result.map_err(io::Error::other)?;
         let seq = &record.seq();
-        if seq.len() < kmer_size {
-            continue;
-        }
-        let packed_seq = PackedSeqVec::from_ascii(seq);
-        let mut fwd_pos = Vec::new();
-        simd_minimizers::minimizer_positions(
-            packed_seq.as_slice(),
-            minimizer_size,
-            window_size,
-            &mut fwd_pos,
-        );
-        for pos in fwd_pos {
-            let shard = packed_seq
-                .slice((pos as usize)..(pos as usize + minimizer_size))
-                .to_word();
-            dict.insert(shard as u64);
-        }
+        match_n
+            .split(seq)
+            .filter(|&seq| seq.len() >= kmer_size)
+            .for_each(|seq| {
+                let packed_seq = PackedSeqVec::from_ascii(seq);
+                let mut fwd_pos = Vec::new();
+                simd_minimizers::minimizer_positions(
+                    packed_seq.as_slice(),
+                    minimizer_size,
+                    window_size,
+                    &mut fwd_pos,
+                );
+                for pos in fwd_pos {
+                    let shard = packed_seq
+                        .slice((pos as usize)..(pos as usize + minimizer_size))
+                        .to_word();
+                    dict.insert(shard as u64);
+                }
+            });
     }
     Ok(dict)
 }
@@ -194,36 +209,46 @@ fn process_query_streaming<P: AsRef<Path>>(
         .map(|n| n.get())
         .unwrap_or(4);
     let mut consumer_handles = Vec::with_capacity(num_consumers);
+    let match_n = Arc::new(
+        RegexBuilder::new(r"[N]+")
+            .case_insensitive(true)
+            .unicode(false)
+            .build()
+            .unwrap(),
+    );
 
     for _ in 0..num_consumers {
         let record_rx_clone = record_rx.clone();
         let result_tx_clone = result_tx.clone();
         let ref_min_dict_clone = Arc::clone(&ref_min_dict);
         let ref_kmer_dict_clone = Arc::clone(&ref_kmer_dict);
+        let match_n_clone = Arc::clone(&match_n);
 
         let handle = thread::spawn(move || {
             while let Ok((id, seq)) = record_rx_clone.recv() {
-                // Compute minimizers for the query sequence using simd-minimizers.
-                let packed_seq = PackedSeqVec::from_ascii(&seq);
-                let mut fwd_pos = Vec::new();
-                simd_minimizers::minimizer_positions(
-                    packed_seq.as_slice(),
-                    minimizer_size,
-                    window_size,
-                    &mut fwd_pos,
-                );
-
                 let mut shared_min_count = 0;
-                for pos in &fwd_pos {
-                    if (*pos as usize + minimizer_size) <= seq.len() {
-                        let minimizer = packed_seq
-                            .slice((*pos as usize)..(*pos as usize + minimizer_size))
-                            .to_word();
-                        if ref_min_dict_clone.contains(&(minimizer as u64)) {
-                            shared_min_count += 1;
+                let mut fwd_pos = Vec::new();
+                match_n_clone
+                    .split(&seq)
+                    .filter(|&seq| seq.len() >= kmer_size)
+                    .for_each(|seq| {
+                        let packed_seq = PackedSeqVec::from_ascii(seq);
+                        fwd_pos.clear();
+                        simd_minimizers::minimizer_positions(
+                            packed_seq.as_slice(),
+                            minimizer_size,
+                            window_size,
+                            &mut fwd_pos,
+                        );
+                        for pos in &fwd_pos {
+                            let minimizer = packed_seq
+                                .slice((*pos as usize)..(*pos as usize + minimizer_size))
+                                .to_word();
+                            if ref_min_dict_clone.contains(&(minimizer as u64)) {
+                                shared_min_count += 1;
+                            }
                         }
-                    }
-                }
+                    });
 
                 if shared_min_count < minimizer_threshold || seq.len() < kmer_size {
                     continue;
